3f5ab7f9be8b012f0d419c4194e91c2d9c42b920
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-11-20 08:26:43 -0600

    FIX: clean up unused files

--- handler.js
+++
@@ -1,7 +0,0 @@
-// This file has been replaced by src/handler.ts
-// The new AWS Lambda implementation is in the src/ directory
-module.exports = {
-  run: async () => {
-    console.log('Please use the new TypeScript implementation in src/');
-  }
-};
